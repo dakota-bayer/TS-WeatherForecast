58643eacdbde6abad5357c8e95f7fcfdaf5a2f63
Dakota Bayer <dakota.bayer@outlook.com>
2024-09-06 05:36:17 -0500

    Remove build files from repository

--- build/forecast.js
+++
@@ -1,8 +0,0 @@
-export class Forecast {
-    constructor(date, summary, temperatureC, temperatureF) {
-        this.date = date;
-        this.summary = summary;
-        this.temperatureC = temperatureC;
-        this.temperatureF = temperatureF;
-    }
-}
